f8020d4605c90e8c9c36fed7710e160fcabd5c93
ddppfamily <ddppfamily@gmail.com>
2017-09-28 18:58:32 +0800

    changed

---
+++ src/test/java/cn/tomoya/XgbbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tomoya;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class XgbbbsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
